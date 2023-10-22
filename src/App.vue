745a0e655adcc0cc95687fc211069712e77d5358
Arjun Pandi <gmarjun100@gmail.com>
2023-10-23 00:15:36 +0530

    Added Layout - 3

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,24 @@
       <div class="p-6 h-full">
         <section class="styled p-1 h-full">
           <div class="list border-2 rounded border-blue-600 h-full p-2">
+            <h1 class="text-center text-xl font-bold mb-[100px]">Title</h1>
+            <div class="grid grid-cols-2 gap-20 mb-8">
+              <div class="flex gap-2">
+                <label for="name" class="text-base font-bold">பெயர்</label>
+                <input type="text"
+                  class="w-full h-full border-b-2 border-dotted border-blue-600 focus:outline-none text-center text-sm text-black">
+              </div>
+              <div class="flex gap-2">
+                <label for="name" class="text-base font-bold">நாள்</label>
+                <input type="text"
+                  class="w-full h-full border-b-2 border-dotted border-blue-600 focus:outline-none text-center text-sm text-black">
+              </div>
+            </div>
             <div class="flex justify-center">
               <div class="bg-blue-600 text-white px-3 py-1 mb-2 text-sm rounded font-bold"> காய்கறி சிட்டை</div>
             </div>
-            <div class="grid grid-cols-[1fr_50px_50px_1fr_50px_50px] border-2 border-blue-600 divide-x-2 divide-blue-600 h-6">
+            <div
+              class="grid grid-cols-[1fr_50px_50px_1fr_50px_50px] border-2 border-blue-600 divide-x-2 divide-blue-600 h-6">
               <div class="h-full flex items-center px-3 font-bold"></div>
               <div class="h-full flex items-center justify-center font-bold w-full">
                 கிலோ
@@ -23,7 +37,8 @@
                 கிராம்
               </div>
             </div>
-            <div class="grid grid-cols-[1fr_50px_50px_1fr_50px_50px] border-2 border-blue-600 divide-x-2 divide-blue-600 h-6"
+            <div
+              class="grid grid-cols-[1fr_50px_50px_1fr_50px_50px] border-2 border-blue-600 divide-x-2 divide-blue-600 h-6"
               v-for="(item, i) in  vegList " :key="i">
               <div class="h-full flex items-center px-3 font-bold">{{ item[0] }}</div>
               <div class="h-full flex items-center">
@@ -77,7 +92,7 @@ onMounted(() => {
       transparent 20px) 20/1rem;
 }
 
-.list :not(div:last-child) {
+.list div:not(:last-child) {
   border-bottom: 0px;
 }
 </style>
