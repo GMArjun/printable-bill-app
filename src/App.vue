ab295139e3ad97c9acf6dcee83eb1b84de20f245
Arjun Pandi <gmarjun100@gmail.com>
2023-10-22 22:41:07 +0530

    Added  Electron and Tailwind

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, eius.
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "p") {
+      event.preventDefault();
+      window.print();
+    }
+  });
+});
+</script>
